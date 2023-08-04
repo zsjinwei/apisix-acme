3986283cb0475b7b45d32cd5483232c7cd23cfc0
huangjinwei <zsjinwei@foxmail.com>
2023-08-04 14:15:53 +0800

    适配apisix admin v3 api

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
-const APISIX_HOST = process.env.APISIX_HOST || 'http://apisix:9080'; // apisix host
-const APISIX_TOKEN = process.env.APISIX_TOKEN || ''; // apisix token
-const ACME_EMAIL = process.env.ACME_EMAIL || ''; // default acme mail
-const ACME_PARAMS = process.env.ACME_PARAMS || ''; // default acme params
+const APISIX_HOST = process.env.APISIX_HOST || 'http://127.0.0.1:9180'; // apisix host
+const APISIX_TOKEN = process.env.APISIX_TOKEN || '90b22855322c3b8488e76b8994a40207'; // apisix token
+const APISIX_API_VERSION = process.env.APISIX_API_VERSION || 'v3'; // apisix api version, default is v2, latest is v3
+const ACME_EMAIL = process.env.ACME_EMAIL || 'zsjinwei@foxmail.com'; // default acme mail
+const ACME_PARAMS = process.env.ACME_PARAMS || '--dns dns_ali --server letsencrypt --force'; // default acme params
 const CERT_EXPIRE_DAYS = process.env.CERT_EXPIRE_DAYS || '14'; // 离过期时间多少天开始刷新证书
 const RENEW_CRONTAB = process.env.RENEW_CRONTAB || '0 0 3 * * *'; // renew crontab
 const CHECK_DOMAIN_CRONTAB = process.env.CHECK_DOMAIN_CRONTAB || '*/1 * * * *'; // check domain crontab
@@ -9,6 +10,7 @@ const CHECK_DOMAIN_CRONTAB = process.env.CHECK_DOMAIN_CRONTAB || '*/1 * * * *'; 
 module.exports = {
   APISIX_HOST,
   APISIX_TOKEN,
+  APISIX_API_VERSION,
   ACME_EMAIL,
   ACME_PARAMS,
   CERT_EXPIRE_DAYS,
